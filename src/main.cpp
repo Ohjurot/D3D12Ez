33445e4cc771e12706c01a90d8893027750bb5ab
Ludwig Füchsl <ludwig@fuechsl.org>
2023-07-26 21:27:42 +0200

    ID3D12Resource and copying

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,54 @@ int main()
     DXDebugLayer::Get().Init();
     if (DXContext::Get().Init() && DXWindow::Get().Init())
     {
+        const char* hello = "Hello World!";
+
+        D3D12_HEAP_PROPERTIES hpUpload{};
+        hpUpload.Type = D3D12_HEAP_TYPE_UPLOAD;
+        hpUpload.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+        hpUpload.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+        hpUpload.CreationNodeMask = 0;
+        hpUpload.VisibleNodeMask = 0;
+
+        D3D12_HEAP_PROPERTIES hpDefault{};
+        hpDefault.Type = D3D12_HEAP_TYPE_DEFAULT;
+        hpDefault.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+        hpDefault.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+        hpDefault.CreationNodeMask = 0;
+        hpDefault.VisibleNodeMask = 0;
+
+        D3D12_RESOURCE_DESC rd{};
+        rd.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+        rd.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+        rd.Width = 1024;
+        rd.Height = 1;
+        rd.DepthOrArraySize = 1;
+        rd.MipLevels = 1;
+        rd.Format = DXGI_FORMAT_UNKNOWN;
+        rd.SampleDesc.Count = 1;
+        rd.SampleDesc.Quality = 0;
+        rd.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+        rd.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+        ComPointer<ID3D12Resource2> uploadBuffer, vertexBuffer;
+        DXContext::Get().GetDevice()->CreateCommittedResource(&hpUpload, D3D12_HEAP_FLAG_NONE, &rd, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&uploadBuffer));
+        DXContext::Get().GetDevice()->CreateCommittedResource(&hpDefault, D3D12_HEAP_FLAG_NONE, &rd, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&vertexBuffer));
+
+        // Copy void* --> CPU Resource
+        void* uploadBufferAddress;
+        D3D12_RANGE uploadRange;
+        uploadRange.Begin = 0;
+        uploadRange.End = 1023;
+        uploadBuffer->Map(0, &uploadRange, &uploadBufferAddress);
+        memcpy(uploadBufferAddress, hello, strlen(hello) + 1);
+        uploadBuffer->Unmap(0, &uploadRange);
+
+        // Copy CPU Resource --> GPU Resource
+        auto* cmdList = DXContext::Get().InitCommandList();
+        cmdList->CopyBufferRegion(vertexBuffer, 0, uploadBuffer, 0, 1024);
+        DXContext::Get().ExecuteCommandList();
+
+
         DXWindow::Get().SetFullscreen(true);
         while (!DXWindow::Get().ShouldClose())
         {
@@ -27,7 +75,7 @@ int main()
             }
             
             // Begin drawing
-            auto* cmdList = DXContext::Get().InitCommandList();
+            cmdList = DXContext::Get().InitCommandList();
             
             // Draw to window
             DXWindow::Get().BeginFrame(cmdList);
