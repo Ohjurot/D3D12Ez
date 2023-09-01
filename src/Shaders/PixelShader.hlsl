df860bce3bdb9da09f24ace70a7400ea1e13c4b3
Ludwig Füchsl <ludwig@fuechsl.org>
2023-09-01 17:23:32 +0200

    Root SIgnature and Textures

--- src/Shaders/PixelShader.hlsl
+++ src/Shaders/PixelShader.hlsl
@@ -1,9 +1,12 @@
 #include "RootSignature.hlsl"
 
 float3 color : register(b0);
+Texture2D<float4> textures[] : register(t0);
+sampler textureSampler : register(s0);
 
 [RootSignature(ROOTSIG)]
 float4 main() : SV_Target
 {
-    return float4(color, 1.0f);
+    float4 texel = textures[0].Sample(textureSampler, float2(0.5f, 0.5f));
+    return float4(texel.rgb, 1.0f);
 }
--- src/Shaders/RootSignature.hlsl
+++ src/Shaders/RootSignature.hlsl
@@ -1,11 +1,18 @@
 /*
 *   Root Signature Layout (CPU)
-*   - 0: float3 "Color"
+*   - 0     float3 "Color"
+*   - 1     descriptor tabel "Textures"
 *   
 *   Root Signature Layout (GPU)
-*   - b0: float3 "Color"
+*   - b0        float3 "Color"
+*   - t0...     Texture2D<float4> "Textures"
+*   - s0        Sampler for "Textures"
 */
 
 #define ROOTSIG \
 "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT)," \
-"RootConstants(num32BitConstants=3, b0)"
+"RootConstants(num32BitConstants=3, b0)," \
+"DescriptorTable(" \
+    "SRV(t0, numDescriptors = unbounded)" \
+")," \
+"StaticSampler(s0)"
